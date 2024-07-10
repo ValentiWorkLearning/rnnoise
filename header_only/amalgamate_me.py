ca7a5aa5d4fe44a8d8968f50d343518bf200e8dc
Valentyn Korniienko <25596072+ValentiWorkLearning@users.noreply.github.com>
2024-07-10 17:22:06 +0300

    Adjusted amalgamation script and github actions job

--- header_only/amalgamate_me.py
+++ header_only/amalgamate_me.py
@@ -3,70 +3,86 @@
 import pathlib
 import subprocess
 import os
+import argparse
+from dataclasses import dataclass
 
-SOURCE_PATH = pathlib.Path(os.getcwd(), "amalgamation", "Heady")
-BUILD_PATH = pathlib.Path(os.getcwd(), "amalgamation", "Heady", "build")
+@dataclass
+class HeadyPackage:
+    source_directory:pathlib.Path
+
+    def amalgamate_sources(self, rnnoise_sources_path:pathlib.Path):
+        self._build_if_necessary()
+
+        command = [
+            f"{pathlib.Path(self.build_directory,'Heady')}",
+            "-s",
+            "src",
+            "-e",
+            "config.h compile.sh rnn_train.py write_weights.c rnnoise_data.c vec_avx.h",
+            "-o",
+            "header_only/rnnoise_amalgamated.c",
+            "-d",
+            "RNNOISE_ALMAGAMATED",
+        ]
+
+        try:
+            result = subprocess.run(
+                command,
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                cwd=rnnoise_sources_path
+            )
+            print("Command executed successfully.")
+            print("Output:\n", result.stdout)
+        except subprocess.CalledProcessError as e:
+            print("Error occurred while executing the command.")
+            print("Error message:\n", e.stderr)
 
 
-def build_if_necessary():
-    cmake_configure = [
-        "cmake",
-        "-G",
-        "Unix Makefiles",
-        f"-S={SOURCE_PATH}",
-        f"-B={BUILD_PATH}",
-    ]
-    subprocess.run(
-        cmake_configure,
-        check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-    )
-
-    cmake_build = [
-        "cmake",
-        "--build",
-        f"{pathlib.Path(os.getcwd(),'amalgamation','Heady','build')}",
-    ]
-
-    subprocess.run(
-        cmake_build,
-        check=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-    )
-
-
-def run_shell_command():
-    command = [
-        f"{pathlib.Path(BUILD_PATH,'Heady')}",
-        "-s",
-        "src",
-        "-e",
-        "config.h compile.sh rnn_train.py write_weights.c rnnoise_data.c vec_avx.h",
-        "-o",
-        "header_only/rnnoise_amalgamated.c",
-        "-d",
-        "RNNOISE_ALMAGAMATED",
-    ]
-
-    try:
-        result = subprocess.run(
-            command,
+    def _build_if_necessary(self):
+        self.build_directory = pathlib.Path(self.source_directory,"build")
+        cmake_configure = [
+            "cmake",
+            "-G",
+            "Unix Makefiles",
+            f"-S={self.source_directory}",
+            f"-B={self.build_directory}",
+        ]
+        subprocess.run(
+            cmake_configure,
             check=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
         )
-        print("Command executed successfully.")
-        print("Output:\n", result.stdout)
-    except subprocess.CalledProcessError as e:
-        print("Error occurred while executing the command.")
-        print("Error message:\n", e.stderr)
 
+        cmake_build = [
+            "cmake",
+            "--build",
+            f"{self.build_directory}",
+        ]
+
+        subprocess.run(
+            cmake_build,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Rnnoise amalgamation tool based on Heady package")
+    
+    parser.add_argument("--heady-source", type=pathlib.Path, help="Source directory of Heady package")
+    parser.add_argument("--rnnoise-source", type=pathlib.Path, help="Source directory with rnnoise sources")
+    
+    args = parser.parse_args()
+    
+    heady_amalgamation_tool = HeadyPackage(source_directory=args.heady_source)
+    heady_amalgamation_tool.amalgamate_sources(args.rnnoise_source)
 
 if __name__ == "__main__":
-    build_if_necessary()
-    run_shell_command()
+    main()
