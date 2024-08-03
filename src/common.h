b5be565ad912d7cc211869138d888b062099f10f
Valentyn Korniienko <25596072+ValentiWorkLearning@users.noreply.github.com>
2024-08-03 18:26:41 +0300

    Removed usage of malloc at all

--- src/common.h
+++ src/common.h
@@ -13,31 +13,6 @@
 #define RNN_INLINE inline
 #define OPUS_INLINE inline
 
-/** RNNoise wrapper for malloc(). To do your own dynamic allocation, all you need t
-o do is replace this function and rnnoise_free */
-#ifndef OVERRIDE_RNNOISE_ALLOC
-static RNN_INLINE void *rnnoise_alloc (size_t size)
-{
-#ifdef USE_MIMALLOC_ALLOCATOR
-   return mi_malloc(size);
-#else
-   return malloc(size);
-#endif
-}
-#endif
-
-/** RNNoise wrapper for free(). To do your own dynamic allocation, all you need to do is replace this function and rnnoise_alloc */
-#ifndef OVERRIDE_RNNOISE_FREE
-static RNN_INLINE void rnnoise_free (void *ptr)
-{
-#ifdef USE_MIMALLOC_ALLOCATOR
-   mi_free(ptr);
-#else
-   free(ptr);
-#endif
-}
-#endif
-
 /** Copy n elements from src to dst. The 0* term provides compile-time type checking  */
 #ifndef OVERRIDE_RNN_COPY
 #define RNN_COPY(dst, src, n) (memcpy((dst), (src), (n)*sizeof(*(dst)) + 0*((dst)-(src)) ))
